fd149f6d6a34bc71dd91f60c417c96cbbbbd6448
pulkitSharma <pulkitsharma1816@gmail.com>
2026-01-24 23:39:40 +0530

    react library and jsx

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,9 +1,10 @@
 import Chai from "./chai";
 function App() {
+  const username = "chai aur code";
   return (
     <>
       <Chai />
-      <h1>Chai aur react with vite</h1>
+      <h1>Chai aur react with vite {username}</h1>
     </>
   );
 }
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,4 +1,40 @@
 import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(<App />);
+function MyApp() {
+  return (
+    <div>
+      <h1>Custom App !</h1>
+    </div>
+  );
+}
+
+// const ReactElement = {
+//   type: "a",
+//   props: {
+//     href: "https://google.com",
+//     target: "_blank",
+//   },
+//   children: "Click me to visit Google",
+// };
+
+const anotherUser = "chai aur react";
+const anotherElement = (
+  <a href="https://www.google.com" target="_blank">
+    Visit Google
+  </a>
+);
+
+const ReactElement = React.createElement(
+  "a",
+  {
+    href: "https://www.google.com",
+    target: "_blank",
+  },
+  "click me to visit Google",
+  anotherUser,
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(ReactElement);
