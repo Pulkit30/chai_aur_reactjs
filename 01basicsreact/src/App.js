58fd2026482b5e84efd09a88c015ea60e968f906
pulkitSharma <pulkitsharma1816@gmail.com>
2026-01-24 22:29:50 +0530

    react flow and structure

---
+++ 01basicsreact/src/App.js
@@ -0,0 +1,11 @@
+import Chai from "./chai";
+function App() {
+  return (
+    <>
+      <Chai />
+      <h1>Chai Aur Reactjs</h1>;
+    </>
+  );
+}
+
+export default App;
---
+++ 01basicsreact/src/chai.js
@@ -0,0 +1,4 @@
+function Chai() {
+  return <h2>Chai in reactjs</h2>;
+}
+export default Chai;
